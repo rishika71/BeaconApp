147e388eec1906eb520194f5fc794764e07d5c5f
Rishika Mathur <rishika71@gmail.com>
2021-11-10 22:55:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/beaconapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.beaconapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bluecats.sdk.BCBeacon;
+import com.bluecats.sdk.BCBeaconManager;
+import com.bluecats.sdk.BCBeaconManagerCallback;
+import com.bluecats.sdk.BCSite;
+import com.bluecats.sdk.BlueCatsSDK;
+
+import java.net.URL;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String TOKEN = "";
+    BCBeaconManager beaconManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BlueCatsSDK.startPurringWithAppToken(getApplicationContext(), TOKEN);
+
+        beaconManager = new BCBeaconManager();
+        beaconManager.registerCallback( mBeaconManagerCallback );
+
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        BlueCatsSDK.didEnterForeground();
+        beaconManager.registerCallback( mBeaconManagerCallback );
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        BlueCatsSDK.didEnterBackground();
+        beaconManager.unregisterCallback( mBeaconManagerCallback );
+    }
+
+    private BCBeaconManagerCallback mBeaconManagerCallback = new BCBeaconManagerCallback()
+    {
+        @Override
+        public void didEnterSite( final BCSite site ) {}
+        
+    };
+
+}
