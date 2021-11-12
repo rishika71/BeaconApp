d825e8a5ea4b47fe621f56af80d289c3783bca44
Rishika Mathur <rishika71@gmail.com>
2021-11-12 12:31:51 -0500

    -- login and register fragment

--- app/src/main/java/com/example/beaconapp/MainActivity.java
+++ app/src/main/java/com/example/beaconapp/MainActivity.java
@@ -1,35 +1,58 @@
 package com.example.beaconapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.bluecats.sdk.BCBeacon;
 import com.bluecats.sdk.BCBeaconManager;
 import com.bluecats.sdk.BCBeaconManagerCallback;
 import com.bluecats.sdk.BCSite;
 import com.bluecats.sdk.BlueCatsSDK;
+import com.example.beaconapp.models.User;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoginFragment.ILogin, RegisterFragment.IRegister {
 
-    private final static String TOKEN = "06e8c088-fae4-419c-aeb6-c56e8def1c42";
+    private final OkHttpClient client = new OkHttpClient();
+    public static final String BASE_URL  = "https://mysterious-beach-05426.herokuapp.com/"; // http://10.0.2.2:3000/
+    private static final String BEACON_TOKEN = "06e8c088-fae4-419c-aeb6-c56e8def1c42";
+
     BCBeaconManager beaconManager;
+    ProgressDialog dialog;
+    User user = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BlueCatsSDK.startPurringWithAppToken(getApplicationContext(), TOKEN);
+        BlueCatsSDK.startPurringWithAppToken(getApplicationContext(), BEACON_TOKEN);
 
         beaconManager = new BCBeaconManager();
         beaconManager.registerCallback( mBeaconManagerCallback );
 
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerLayout, new LoginFragment())
+                .commit();
     }
-
 
     @Override
     protected void onResume()
@@ -49,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
         beaconManager.unregisterCallback( mBeaconManagerCallback );
     }
 
+
+
     private BCBeaconManagerCallback mBeaconManagerCallback = new BCBeaconManagerCallback()
     {
         @Override
@@ -56,4 +81,147 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    @Override
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public void alert(String msg) {
+        runOnUiThread(() -> new AlertDialog.Builder(this)
+                .setTitle("Info")
+                .setMessage(msg)
+                .setPositiveButton("Okay", null)
+                .show());
+    }
+
+    @Override
+    public void goBack() {
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @Override
+    public void sendRegisterView() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerLayout, new RegisterFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void sendProductsView() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerLayout, new ProductFragment())
+                .commit();
+    }
+
+    public void clientToken(Return response){
+        FormBody formBody = new FormBody.Builder()
+                .add("customerId", user.getCustomerId())
+                .build();
+        Request request = new Request.Builder()
+                .url(BASE_URL + "product/clienttoken")
+                .addHeader("x-jwt-token", user.getToken())
+                .post(formBody)
+                .build();
+        sendRequest(request, response);
+    }
+
+    public void register(Return response, String... data){
+        FormBody formBody = new FormBody.Builder()
+                .add("fullname", data[0])
+                .add("address", data[1])
+                .add("email", data[2])
+                .add("pass", data[3])
+                .build();
+        Request request = new Request.Builder()
+                .url(BASE_URL + "auth/signup")
+                .post(formBody)
+                .build();
+        sendRequest(request, response);
+    }
+
+    public void login(Return response, String... data){
+        FormBody formBody = new FormBody.Builder()
+                .add("email", data[0])
+                .add("pass", data[1])
+                .build();
+        Request request = new Request.Builder()
+                .url(BASE_URL + "auth/login")
+                .post(formBody)
+                .build();
+        sendRequest(request, response);
+    }
+
+    private void sendRequest(Request request, Return callback) {
+        if(callback.showDialog()) toggleDialog(true, "Processing...");
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                if(callback.showDialog()) toggleDialog(false, null);
+                e.printStackTrace();
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(callback.showDialog()) toggleDialog(false, null);
+
+                ResponseBody responseBody = response.body();
+
+                String res_string;
+                if (responseBody != null) {
+                    res_string = responseBody.string();
+
+                    if (response.isSuccessful()) {
+                        runOnUiThread(() -> callback.response(res_string));
+                    } else {
+                        runOnUiThread(() -> {
+                            try {
+                                JSONObject jsonObject = new JSONObject(res_string);
+                                if (jsonObject.has("message"))
+                                    alert(jsonObject.getString("message"));
+                            }catch (JSONException exc){
+                            }
+                        });
+                        callback.error(res_string);
+                    }
+                }
+            }
+        });
+    }
+
+    interface Return{
+
+        void response(@NotNull String response);
+
+        void error(@NotNull String response);
+
+        boolean showDialog();
+
+    }
+
+
+    public void toggleDialog(boolean show) {
+        toggleDialog(show, null);
+    }
+
+    public void toggleDialog(boolean show, String msg) {
+        if (show) {
+            dialog = new ProgressDialog(this);
+            if (msg == null)
+                dialog.setMessage("Loading...");
+            else
+                dialog.setMessage(msg);
+            dialog.setCancelable(false);
+            dialog.show();
+        } else {
+            dialog.dismiss();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/beaconapp/RegisterFragment.java
@@ -0,0 +1,106 @@
+package com.example.beaconapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.beaconapp.databinding.FragmentRegisterBinding;
+import com.example.beaconapp.models.User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RegisterFragment extends Fragment {
+
+    FragmentRegisterBinding binding;
+
+    IRegister am;
+
+    String fullname, address, email, pass;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof IRegister) {
+            am = (IRegister) context;
+        } else {
+            throw new RuntimeException(context.toString());
+        }
+    }
+
+    public interface IRegister {
+
+        void setUser(User user);
+
+        void alert(String msg);
+
+        void goBack();
+
+        void sendProductsView();
+
+        void register(com.example.beaconapp.MainActivity.Return response, String... data);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getActivity().setTitle("Register");
+
+        binding = FragmentRegisterBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.cancelButtonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                am.goBack();
+            }
+        });
+
+        binding.registerButtonId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fullname = binding.edittext.getText().toString();
+                address = binding.edittext4.getText().toString();
+                email = binding.edittext5.getText().toString();
+                pass = binding.edittext6.getText().toString();
+
+                if(email.isEmpty() || fullname.isEmpty() || address.isEmpty() || pass.isEmpty()){
+                    am.alert("Please enter all values for registering!");
+                    return;
+                }
+
+
+                am.register(new com.example.beaconapp.MainActivity.Return() {
+                    @Override
+                    public void response(@NotNull String response) {
+                        GsonBuilder builder = new GsonBuilder();
+                        Gson gson = builder.create();
+                        User user = gson.fromJson(response, User.class);
+                        am.setUser(user);
+                        am.sendProductsView();
+                    }
+
+                    @Override
+                    public boolean showDialog() {
+                        return true;
+                    }
+
+                    @Override
+                    public void error(@NotNull String response) {
+                    }
+                }, fullname, address, email, pass);
+
+            }
+        });
+
+        return view;
+    }
+}
